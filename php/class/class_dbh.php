ed3d0da13c7412ac3b2d9d07c521ecf9902ca2e7
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-02-08 14:18:10 +0100

    Update class_dbh.php

---
+++ php/class/class_dbh.php
@@ -0,0 +1,31 @@
+<?php
+//connexion à la base de donnée
+class Dbh
+{
+    //Methode permettant la connexion à la BDD
+    protected function connect()
+    {
+        
+        try {
+
+            $username = "root";
+            $password = "root";
+            $dbh = new PDO('mysql:host=localhost:8889; dbname=blog', $username, $password);
+            return $dbh;
+            
+        } catch (PDOException $e) {
+            print "Error! :" . $e->getMessage() . "<br/>";
+            die();
+        }
+
+    }
+
+    protected function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+}
