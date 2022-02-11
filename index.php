015ce06bd7139eadd7d5e21cc9ec38250bfad9e1
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-02-11 11:02:04 +0100

    getIndexArticles désormais fonctionnelle
    l'index affiche désormais les trois articles les plus récents

--- index.php
+++ index.php
@@ -6,29 +6,15 @@
     require ('php/include/header.inc.php'); 
 
     $article = new Article();
-    $text = $article->getIndexArticles();
+    
 ?>
     <main>
 
-        <h1 id="titre-presentation">Bienvenue</h1>
+<?php
+$text = $article->getIndexArticles();
+// var_dump($text);
 
-        <div class="containerA1">
-
-            <?php        
-                // $article = explode('/', $text['0']['article']);
-
-            ?>
-
-            <h2><?php echo $article[0]; ?></h2>
-
-            <p>
-
-                <?php echo $article[1]; ?>
-
-            </p>
-
-        </div>
-        
+?>
     </main>
 
 
