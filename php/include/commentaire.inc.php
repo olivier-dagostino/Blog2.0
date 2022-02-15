499a9742410802824289c5556e7827a19c98c341
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-02-15 11:32:23 +0100

    modif mineures

--- php/include/commentaire.inc.php
+++ php/include/commentaire.inc.php
@@ -6,16 +6,19 @@ require('autoloader.inc.php');
 
 if (isset($_POST['submit'])) {
 
-    if (empty($_POST['titre']) || empty($_POST['corp'])) {
+    if (empty($_POST['titre']) || empty($_POST['commentaire'])) {
 
         echo "<p>Veuillez remplir tout les champs</p>";
 
     } else {
 
-        $corp = $_POST['titre'] . '/' . $_POST['corp'];
-        $comm->insertcom($corp, $_GET['id'], $_SESSION['id']);
+        $commentaire = new Commentaire();
+
+        $commentaire->insertCommentaire($_POST['titre'], $_POST['commentaire'], $_SESSION['id'], $_POST['id_article']);
+
         echo "<p>Votre commentaire est bien enregistré</p>";
-        header("Refresh:2, URL=article.php?id=$id");
+
+        header("Refresh:2, URL=../../article.php?id=" . $_POST["id_article"]);
 
     }
 }                
