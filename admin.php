cfa8c34f8d9f95f2c0222253d52286e69ab87b9a
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-02-16 16:49:05 +0100

    ajout message erreur/succès admin

--- admin.php
+++ admin.php
@@ -22,6 +22,20 @@ if ($_SESSION['droits'] != 1337) {
 ?>
     <main>
 
+        <aside>
+                <?php 
+                if(isset($_SESSION["error"])) {
+                    echo $_SESSION["error"];
+                    unset($_SESSION["error"]);
+                }
+                
+                if (isset($_SESSION["success"])){
+                    echo $_SESSION["success"];
+                    unset($_SESSION["success"]);
+                }
+                ?>
+        </aside>
+
         <section class="gestion-user">
 
             <h1>Gestion des Utilisateurs</h1>
--- php/include/admin.inc.php
+++ php/include/admin.inc.php
@@ -5,30 +5,56 @@ session_start();
 require('autoloader.inc.php');
 
 $user = new User();
-$cat = new Categorie();
 $article = new Article();
+$categorie = new Categorie();
 
 switch ($_POST) {
 
+    // Suppression d'utilisateur
     case isset($_POST['delete_user']):
 
-        $res1 = $user->deleteUser($value['id']);
-        header('location: ../../index.php');
+        // Vérification que l'utilisateur en question n'est pas l'admin connecté
+        if ($_POST['id'] != $_SESSION['id']) {
+            
+            $res1 = $user->deleteUser($_POST['id']);
+            header('location: ../../admin.php');
+
+        } else {
+
+            $_SESSION['error'] = "Vous ne pouvez pas supprimer votre compte";
+            header('location: ../../admin.php');
+            break;
+        }
+        
         break;
     
+    // Modification des droits d'un utilisateur
     case isset($_POST['modif_droit']);
 
-        $user->setDroit($_POST['select-droits'], $_POST['id']);
-        header('location: ../../admin.php');
+        // Vérification que l'utilisateur en question n'est pas l'admin connecté
+        if ($_POST['id'] != $_SESSION['id']) {
+
+            $user->setDroit($_POST['select-droits'], $_POST['id']);
+            header('location: ../../index.php');
+            
+        } else {
+
+            $_SESSION['error'] = "Vous ne pouvez pas modifier vos droits";
+            header('location: ../../admin.php');
+            break;
+        }
+
         break;
 
+    // Modification de la catégorie d'un article
     case isset($_POST['modif_categorie']);
         
         $article->setCategorie($_POST['id_categorie'], $_POST['id_article']);
-        echo "ok categorie";
+        $_SESSION['success'] = "Modification réussie";
         header(('location: ../../admin.php'));
         break;
 
+    // Suppression d'un article
     case isset($_POST['delete_article']);
 
         echo "ok suppressions";
