92764d35e0f4bfe9e62a90bfa1624f3d3e625205
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-02-16 08:57:47 +0100

    style

--- admin.php
+++ admin.php
@@ -14,70 +14,70 @@ if ($_SESSION['droits'] != 1337) {
 ?>
     <main>
 
-        <h1>Gestion des Utilisateurs</h1>
+        <section class="container">
+            
+            <table>
+                <h1>Gestion des Utilisateurs</h1>
 
-        <table>
+                <thead>
 
-            <thead>
+                    <?php
 
-                <?php
+                    $info = new User();
+                    $res1 = $info->getAllInfoForAllUsers();
 
-                $info = new User();
-                $res1 = $info->getAllInfoForAllUsers();
+                    ?>
+                    
+                </thead>
 
-                ?>
-                
-            </thead>
-
-            <tbody>
+                <tbody>
 
 
-                <?php
+                    <?php
 
-                foreach ($res1 as $key => $value) { // pour chaque valeur qui se trouve dans la $res1 
-                    echo '<tr>'; //on echo une ligne
+                    foreach ($res1 as $key => $value) { // pour chaque valeur qui se trouve dans la $res1 
+                        echo '<tr>'; //on echo une ligne
 
-                    foreach ($value as $key1 => $value1) { //Pour chaque info de l'étudiant on écho une case
+                        foreach ($value as $key1 => $value1) { //Pour chaque info de l'étudiant on écho une case
 
-                        echo "<td>$value1</td>";
-                        // j'echo l'info
+                            echo "<td>$value1</td>";
+                            // j'echo l'info
 
-                    }
+                        }
 
-                    echo"<td>
-                            <form action='php/include/admin.inc.php' method='POST'>
+                        echo"<td>
+                                <form action='php/include/admin.inc.php' method='POST'>
 
-                                <input type='text' name='id' value='" . $value['id'] . "' hidden>
+                                    <input type='text' name='id' value='" . $value['id'] . "' hidden>
 
-                                <select name='select-droits' id='select-droitd'>
+                                    <select name='select-droits' id='select-droitd'>
 
-                                    <option>--Droits--</option>
-                                    <option value='1'>Utilisateurs</option>
-                                    <option value='42'>Modérateur</option>
-                                    <option value='1337'>Admin</option>
+                                        <option>--Droits--</option>
+                                        <option value='1'>Utilisateurs</option>
+                                        <option value='42'>Modérateur</option>
+                                        <option value='1337'>Admin</option>
 
-                                </select>
-                            
-                                <button type='submit' name='submit'>Modifier les Droits</button>
-
-                                <button type='submit' name='delete'>Supprimer</button>
-
-                            </form>
+                                    </select>
                                 
-                        </td>";
+                                    <button type='submit' name='submit'>Modifier les Droits</button>
 
-                    echo '</tr>';
-                }
-                
-                
-                
-                ?>
+                                    <button type='submit' name='delete'>Supprimer</button>
+
+                                </form>
+                                    
+                            </td>";
+
+                        echo '</tr>';
+                    }
+                    
+                    ?>
 
 
-                <tr></tr>
+                </tbody>
 
-            </tbody>
-        </table>
+            </table>
+
+        </section>
 
     </main>
 
