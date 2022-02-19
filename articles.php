b36fd172e5f2fd574eda74dbcd3921b957af0c30
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-02-19 14:30:54 +0100

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,27 +1,29 @@
 <?php
 
-    session_start();
-    $title = "Articles";
-    $css = "article";
-    require ('php/include/header.php');
+session_start();
+$title = "Articles";
+$css = "article";
+require('php/include/header.php');
 
-    $article = new Article();
+$article = new Article();
 
-    $categorie = new Categorie();
+$categorie = new Categorie();
 
 ?>
 
 <main>
-    
+
     <?php
 
-        // On détermine sur quelle page on se trouve
-        if(isset($_GET['start']) && !empty($_GET['start'])){
+    $pages = $article->totalPages();
 
-            $currentPage = (int) strip_tags($_GET['start']);
+    // On détermine sur quelle page on se trouve
+    if (isset($_GET['start']) && !empty($_GET['start'])) {
 
-            if(isset($_GET['categorie']) && !empty($_GET['categorie'])){
-            
+        $currentPage = (int) strip_tags($_GET['start']);
+
+        if (isset($_GET['categorie']) && !empty($_GET['categorie'])) {
+
             // Calcul du 1er article de la page
             $start = $currentPage * 5 - 5;
 
@@ -29,69 +31,57 @@
 
             echo "<h1>" . $categories[0]['nom'] . "</h1>";
 
-            $articles= $article->getArticles(5, $start, $_GET['categorie']);
+            $articles = $article->getArticles(5, $start, $_GET['categorie']);
+        } else {
 
-            }else{
+            $currentPage = (int) strip_tags($_GET['start']);
 
-                $currentPage = (int) strip_tags($_GET['start']);
+            $start = $currentPage * 5 - 5;
 
-                $start = $currentPage * 5 - 5;
+            echo "<h1>Articles les plus récents</h1>";
 
-                echo "<h1>Articles les plus récents</h1>";
-
-                $articles= $article->getArticles(5, $start, '');
-
-            }
-
-        }else{
-
-            $currentPage = 1;
-
-            if(isset($_GET['categorie']) && !empty($_GET['categorie'])){
-
-                $categories = $categorie->getAllInfoById($_GET['categorie']);
-
-                echo "<h1>" . $categories[0]['nom'] . "</h1>";    
-                
-                $articles= $article->getArticles(5, 0, $_GET['categorie']);
-    
-                }else{
-
-                    echo "<h1>Articles les plus récents</h1>";
-        
-                    $articles= $article->getArticles(5, 0);
-    
-                }    
-
+            $articles = $article->getArticles(5, $start, '');
         }
+    } else {
+
+        $currentPage = 1;
+
+        if (isset($_GET['categorie']) && !empty($_GET['categorie'])) {
+
+            $categories = $categorie->getAllInfoById($_GET['categorie']);
+
+            echo "<h1>" . $categories[0]['nom'] . "</h1>";
+
+            $articles = $article->getArticles(5, 0, $_GET['categorie']);
+        } else {
+
+            echo "<h1>Articles les plus récents</h1>";
+
+            $articles = $article->getArticles(5, 0);
+        }
+    }
 
     ?>
 
     <nav id="select-page">
 
         <ul class="pagination">
-            <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
             <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
-                <a href="articles.php?start= <?= $currentPage - 1 ?><?= (isset($_GET['categorie'])) ? "&categorie=" . $_GET['categorie'] : "" ?>" class="page-link">Précédente</a>
+                <a href="articles.php?start=<?= $currentPage - 1 ?><?= (isset($_GET['categorie'])) ? "&categorie=" . $_GET['categorie'] : "" ?>" class="page-link">Précédente</a>
             </li>
-
-            <?php $pages = $article->totalPages(); for($page = 1; $page <= $pages; $page++): ?>
-                <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+            <?php for ($page = 1; $page <= $pages; $page++) : ?>
                 <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
                     <a href="articles.php?start=<?= $page ?><?= (isset($_GET['categorie'])) ? "&categorie=" . $_GET['categorie'] : "" ?>" class="page-link"><?= $page ?></a>
                 </li>
             <?php endfor ?>
-
-                <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
-                <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+            <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
                 <a href="articles.php?start=<?= $currentPage + 1 ?><?= (isset($_GET['categorie'])) ? "&categorie=" . $_GET['categorie'] : "" ?>" class="page-link">Suivante</a>
             </li>
         </ul>
-        
+
     </nav>
 
 
 </main>
 
 <?php require('php/include/footer.php'); ?>
-
